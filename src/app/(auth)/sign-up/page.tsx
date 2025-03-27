375a871bdd22a10d979a2501b8c782fff701fc20
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2025-03-28 00:40:12 +0530

    customizing-components and start to create-authscreen

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+export const SignupPage = () => {
+    return (
+        <div>
+            <h1>Sign up page</h1>
+        </div>
+    )
+};
+
+export default SignupPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,31 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>hellow world </div>
+    <div className="">
+      <input />
+
+      <Button size="sm">
+        Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        secondary
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="teritary">
+        Teritary
+      </Button>
+
+    </div>
   );
 }
