27bd1a88ea013ce08839782d228d1380742435af
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2025-04-26 22:29:55 +0530

    Creating workspace members

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,17 +2,36 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { createWorkspaceSchema } from '../schemas';
 import { sessionMiddleware } from '@/lib/session-middleware';
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from '@/conf';
-import { ID } from 'node-appwrite';
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from '@/conf';
+import { ID, Query, Role } from 'node-appwrite';
+import { MemberRole } from '@/features/members/types';
+import { generateInviteCode } from '@/lib/utils';
 
 
 const app = new Hono()
  .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
     const databases = c.get("databases");
+
+     const members = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        [Query.equal("userid",user.$id)]
+     );
+
+     if (members.total === 0) {
+        return c.json({ data: { documents: [], total: 0 } })
+        }
     
+    const workspaceIds = members.documents.map((member) => member.workspaceid);
+
     const workspaces = await databases.listDocuments(
         DATABASE_ID,
-        WORKSPACES_ID
+        WORKSPACES_ID,
+     [
+        Query.orderDesc("$createdAt"),
+        Query.contains("$id", workspaceIds)
+     ],  
     );
     return c.json({ data: workspaces });
  })
@@ -56,7 +75,19 @@ const app = new Hono()
                 name,
                 userid: user.$id,
                 imageUrl: uploadedImageUrl,
+                inviteCode: generateInviteCode(8),
             }, 
+        );
+
+        await databases.createDocument(
+            DATABASE_ID,
+            MEMBERS_ID,
+            ID.unique(),
+            {
+                userid: user.$id,
+                workspaceid: workspace.$id, 
+                role: MemberRole.ADMIN,
+            },
         );
 
         return c.json({ data: workspace });
