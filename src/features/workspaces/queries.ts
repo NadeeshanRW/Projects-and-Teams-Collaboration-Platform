5096245bc039b832b2293ab77222348a1df06fb2
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2025-05-14 09:04:06 +0530

    Refactoring server queris

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,25 +1,16 @@
 
-import { cookies } from "next/headers";
-import { Databases,Client, Query, Account } from "node-appwrite";
-import { AUTH_COOKIE } from "@/features/auth/constants";
+
+import { Query } from "node-appwrite";
+
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
  export const getWorkspaces = async () => {
 
     try{
-    const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-    
-            const session = await cookies().get(AUTH_COOKIE);
-            
-            if (!session) return { documents: [], total: 0 } ;
-            
-            client.setSession (session.value); 
-            const databases = new Databases(client);
-            const account = new Account(client);
+        const { databases , account} =  await createSessionClient();
             const user = await account.get();
 
             const members = await databases.listDocuments(
@@ -55,16 +46,8 @@ interface GetWorkspacesProps{
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspacesProps) => {
    try {
-     const client = new Client()
-       .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-       .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
- 
-     const session = await cookies().get(AUTH_COOKIE);
-     if (!session) return null;
- 
-     client.setSession(session.value);
-     const databases = new Databases(client);
-     const account = new Account(client);
+    const { databases, account } = await createSessionClient();
+
      const user = await account.get();
  
      const member = await getMember({
